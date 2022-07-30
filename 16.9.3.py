d715e957e6e895ca1b90221c139aff5ee42277a2
Iusupovds <105906850+Iusupovds@users.noreply.github.com>
2022-07-30 15:54:48 +0300

    Update 16.9.3.py

--- 16.9.3.py
+++ 16.9.3.py
@@ -7,6 +7,13 @@ class Client:
     def __str__(self):
         return f'"{self.name}. {self.city}. Balance: {self.money} rub."'
 
-Ivan_Petrov = Client('Ivan Petrov', 'Moscow', 50)
+    def get_guests(self):
+        return f'{self.name}, {self.city}'
 
-print(Ivan_Petrov)
+Ivan_Petrov = Client('Ivan Petrov', 'Moscow', 50)
+Sergey_Sidorov = Client('Sergey Sidorov', 'Saint-Petersbourg', 100)
+Marina_Vasilieva = Client('Marina Vasilieva', 'Tula', 0)
+
+List_of_guests = [Ivan_Petrov, Sergey_Sidorov, Marina_Vasilieva]
+for guests in List_of_guests:
+    print(guests.get_guests())
