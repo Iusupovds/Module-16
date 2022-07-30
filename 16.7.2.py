883f0c1e7c564bd5f80236287cc7003e38fb4fef
Iusupovds <105906850+Iusupovds@users.noreply.github.com>
2022-07-30 14:39:28 +0300

    Create 16.7.2.py

---
+++ 16.7.2.py
@@ -0,0 +1,15 @@
+from rectangle import Rectangle
+
+r1 = Rectangle(10,5)
+
+print('r1.width=', r1.width)
+print('r1.height=', r1.height)
+print('r1.getWight=', r1.getWidht())
+print('r1.getArea=', r1.getArea())
+
+r2 = Rectangle(20,10)
+
+print('r2.width=', r2.width)
+print('r2.height=', r2.height)
+print('r2.getWight=', r2.getWidht())
+print('r2.getArea=', r2.getArea())
